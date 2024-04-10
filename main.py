de79664517e6ceee292174ca2b52d2bca0fa94d5
Emre Rasimgil <erasimgil@gmail.com>
2024-04-10 18:22:54 +0200

    implemented logging

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import argparse
 import os
 import shutil
+import sys
 import time
-
+import logging
+logger = logging.getLogger()
 
 def synced(file_source: str, file_replica: str) -> bool:
     return os.path.getmtime(file_source) == os.path.getmtime(file_replica)
 
+def log_operation(operation: chr, item_source: str=None, item_replica: str=None) -> None:
+    match operation:
+        case 'c':
+            logger.info(f"[CREATE] {item_replica}")
+        case 'p':
+            logger.info(f"[COPY] {item_source} > {item_replica}")
+        case 'd':
+            logger.info(f"[DELETE]: {item_replica}")
+
 
 def sync_file(file_source: str, file_replica: str) -> None:
     if not os.path.exists(file_replica):
-        print(f"created file: {file_replica}")
+        log_operation('c', file_replica, item_replica=file_replica)
     if not os.path.exists(file_replica) or not synced(file_source, file_replica):
         os.makedirs(os.path.dirname(file_replica), exist_ok=True)
         shutil.copy2(file_source, file_replica)
-        print(f"synced file: {file_replica} from {file_source}")
-
+        log_operation('p', file_source, file_replica)
 
 def sync_directory(dir_source: str, dir_replica: str) -> None:
     for item in os.listdir(dir_source):
@@ -24,7 +34,7 @@ def sync_directory(dir_source: str, dir_replica: str) -> None:
         if os.path.isdir(item_source):
             if not os.path.exists(item_replica):
                 os.makedirs(item_replica)
-                print(f"created directory {item_replica}")
+                log_operation('c', item_replica=item_replica)
             sync_directory(item_source, item_replica)
         else:
             sync_file(item_source, item_replica)
@@ -35,11 +45,9 @@ def sync_directory(dir_source: str, dir_replica: str) -> None:
         if not os.path.exists(item_source):
             if os.path.isdir(item_replica):
                 shutil.rmtree(item_replica)
-                print(f"deleted directory: {item_replica}")
             else:
                 os.remove(item_replica)
-                print(f"deleted file {item_replica}")
-
+            log_operation('d', item_replica)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -50,13 +58,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if not os.path.isdir(args.source):
-        print("source folder does not exist.")
-        exit()
+        print("Invalid source folder")
+        exit(0)
 
-    if not os.path.isdir(args.replica):
-        print(f"created replica folder: {args.replica}")
-        os.makedirs(args.replica)
-
+    logging.basicConfig(filename=args.output, level=logging.INFO, encoding='utf-8', format='%(asctime)s - %(message)s')
     while True:
-        sync_directory(args.source, args.replica)
-        time.sleep(args.interval)
+        try:
+            sync_directory(args.source, args.replica)
+            time.sleep(args.interval)
+        except KeyboardInterrupt:
+            print("exiting the program")
+            sys.exit(0)
